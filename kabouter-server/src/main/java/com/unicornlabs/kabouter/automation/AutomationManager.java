ef7526c8b9368445af7477318deba4a194d08bfd
MitchJust <>
2012-07-02 12:04:19 +1000

    Initial commit for automation manager

--- kabouter-server/src/main/java/com/unicornlabs/kabouter/automation/AutomationManager.java
+++ kabouter-server/src/main/java/com/unicornlabs/kabouter/automation/AutomationManager.java
@@ -57,7 +57,7 @@ public class AutomationManager {
         
         di.ioStates[ioNum] = value;
         
-        theDeviceManager.updateDevice(di);
+        theDeviceManager.updateDeviceIOState(di);
     }
     
 }
